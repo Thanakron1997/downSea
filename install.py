9d0ae037a7d70f93846a2a1d172e56377720b761
THN1997 <thanakron.nop@gmail.com>
2025-05-26 22:18:23 +0700

    install package

--- install.py
+++ install.py
@@ -1,4 +1,15 @@
+import subprocess
+import sys
 import os
+
+def install_if_missing(package):
+    try:
+        __import__(package)
+        print(f"'{package}' is already installed.")
+    except ImportError:
+        print(f"'{package}' not found. Installing...")
+        subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
 os.system("wget --output-document sratoolkit.tar.gz https://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/current/sratoolkit.current-ubuntu64.tar.gz")
 os.system("tar -vxzf sratoolkit.tar.gz")
 os.system("curl -o datasets 'https://ftp.ncbi.nlm.nih.gov/pub/datasets/command-line/v2/linux-amd64/datasets'")
@@ -8,3 +19,7 @@ list_dir =  [name for name in os.listdir(".") if os.path.isdir(name)]
 for i in list_dir:
     if 'sratoolkit' in i:
         os.rename(i, "sratoolkit")
+
+# Check and install pandas and tqdm
+install_if_missing('pandas')
+install_if_missing('tqdm')
